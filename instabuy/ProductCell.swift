b8b66e2d2b9dde0b22cf68b7f04aeed818176b93
Bruno Lemgruber <brunoteixeiralc@gmail.com>
2017-09-19 13:32:46 -0300

    first commit

---
+++ instabuy/ProductCell.swift
@@ -0,0 +1,28 @@
+//
+//  ParkCell.swift
+//  collectionsviews
+//
+//  Created by Bruno Corrêa on 23/08/17.
+//  Copyright © 2017 Bruno Corrêa. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var productImageView: UIImageView!
+    
+    override func prepareForReuse() {
+        productImageView.image = nil
+    }
+    
+    var product: Product?{
+        didSet {
+            if let p = product{
+                productImageView.image = UIImage(named: "logo")
+            }
+        }
+    }
+    
+}
--- instabuy/ViewController.swift
+++ instabuy/ViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let width = collectionView!.frame.width/2
+        let layout = collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: width, height: width)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
+
+extension ViewController{
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProductCell", for: indexPath) as! ProductCell
+        
+      /*  if let park = parksDataSource.parkForItemAtIndexPath(indexPath){
+            cell.park = park
+        }
+      */
+        
+        return cell
+    }
+}
+
 
