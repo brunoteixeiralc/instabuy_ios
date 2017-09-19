5b27b336f8c11418fabc303760394f147ce9ca5c
Bruno Lemgruber <brunoteixeiralc@gmail.com>
2017-09-19 12:52:05 -0300

    Initial Commit

---
+++ instabuy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instabuy
+//
+//  Created by Bruno Lemgruber on 19/09/17.
+//  Copyright © 2017 Bruno Lemgruber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
